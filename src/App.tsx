67212a6bd9acc7efda7cc6c9916708e7c48b2859
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-15 22:15:31 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 import DashBoard from "./components/dashboard";
 import OnBoardingComponent from "./components/onBoarding";
 
 function App() {
-	const [workspaceId, setWorksapceId] = useState<string|null>("");
-	const [userId, setUserId] = useState<string|null>("");
-	const [toggleDashboard, setToggleDashboard] = useState<boolean>(false)
+	const [workspaceId, setWorksapceId] = useState<string | null>("");
+	const [userId, setUserId] = useState<string | null>("");
+	const [toggleDashboard, setToggleDashboard] = useState<boolean>(false);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	React.useEffect(() => {
-		if(sessionStorage.getItem("workspaceId")&&sessionStorage.getItem("userId")){
+		if (
+			sessionStorage.getItem("workspaceId") &&
+			sessionStorage.getItem("userId")
+		) {
 			setWorksapceId(sessionStorage.getItem("workspaceId"));
 			setUserId(sessionStorage.getItem("userId"));
 			setToggleDashboard(true);
@@ -19,21 +22,24 @@ function App() {
 	}, []);
 
 	return (
-		<div>
-			{isLoading===false?
-				toggleDashboard===false ?
+		<div className="h-full">
+			{isLoading === false ? (
+				toggleDashboard === false ? (
 					<OnBoardingComponent
 						setUserId={setUserId}
 						setWorkspaceId={setWorksapceId}
-						toggleDashboard = {toggleDashboard}
+						toggleDashboard={toggleDashboard}
 						setToggleDashboard={setToggleDashboard}
-					/> :
-					<DashBoard 
+					/>
+				) : (
+					<DashBoard
 						workspaceId={workspaceId}
 						userId={userId}
 					/>
-				:<div>Loading...</div>}
-			
+				)
+			) : (
+				<div>Loading...</div>
+			)}
 		</div>
 	);
 }
