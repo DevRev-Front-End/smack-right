c4740c12eb0a43931385b5b918dba14115c67651
Satyam7Jha <7satyamjha@gmail.com>
2023-02-15 23:20:32 +0530

    add channels

--- src/components/utils/backend.tsx
+++ src/components/utils/backend.tsx
@@ -1,5 +1,12 @@
 import { db } from "../../firebaseConfig";
-import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  setDoc,
+  addDoc,
+  collection,
+} from "firebase/firestore";
 
 // Add a new document with a generated id
 export const addDirectMessage = async (
@@ -21,8 +28,36 @@ export const addDirectMessage = async (
     const res = await updateDoc(channel_collection, {
       conversations: arrayUnion(messageData),
     });
-    console.log("message successfully!");
+    console.log("message sent successfully!");
   } catch (err) {
     console.error(err, "error in addDirectMessage");
   }
+};
+
+
+// creating a new Channel
+export const createChannel = async (
+  workspaceId: string,
+  isPrivate: boolean,
+  name: string
+) => {
+  const channel_initial_data = {
+    conversations: [],
+    users: [],
+  };
+
+  //  initial channel in chat Table
+  const res = await addDoc(collection(db, "channels"), channel_initial_data);
+
+  const washingtonRef = doc(db, "workspace-collection", workspaceId);
+
+  const workspace_chennel_data = {
+    id: res.id,
+    isPrivate: isPrivate,
+    name: name,
+  };
+
+  await updateDoc(washingtonRef, {
+    channels: arrayUnion(workspace_chennel_data),
+  });
 };
