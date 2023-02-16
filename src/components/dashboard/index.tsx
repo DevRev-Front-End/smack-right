31f351ed78b28ddabd5b640f92db8d1289afe18c
Satyam7Jha <7satyamjha@gmail.com>
2023-02-16 11:21:53 +0530

    create workspace function && get workspace lsit funciton

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -3,221 +3,207 @@ import { BiPhoneCall, BiChevronDown } from "react-icons/bi";
 import { FaEdit } from "react-icons/fa";
 import { BsFillLockFill, BsHash } from "react-icons/bs";
 import {
-	BsCaretDownFill,
-	BsCaretRightFill,
-	BsShieldLock,
+  BsCaretDownFill,
+  BsCaretRightFill,
+  BsShieldLock,
 } from "react-icons/bs";
 
 import { Workspace } from "../../types";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import ChatModule from "../chatModule";
 
 export default function DashBoard(props: any) {
-	const [workspace, setWorkspace] = React.useState<any>({
-		id: "",
-		name: "",
-		magicLink: "",
-		admins: [],
-		channels: [],
-		members: [],
-	});
-	const [user, setUser] = React.useState<any>();
-	const [isLoading, setIsLoading] = React.useState<boolean>(false);
-	const [chatData, setChatData] = React.useState<any>();
-	const [isChannelVisible, setIsChannelVisible] =
-		React.useState<boolean>(false);
-	const [isDirectMessageVisible, setIsDirectMessageVisible] =
-		React.useState<boolean>(false);
-	const [selectedChat, setSelectedChat] = React.useState<any>({
-		name: "general",
-		isPrivate: false,
-		id: "sVblA0J0WXMePBS3Chy9",
-	});
+  const [workspace, setWorkspace] = React.useState<any>({
+    id: "",
+    name: "",
+    magicLink: "",
+    admins: [],
+    channels: [],
+    members: [],
+  });
+  const [user, setUser] = React.useState<any>();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [chatData, setChatData] = React.useState<any>();
+  const [isChannelVisible, setIsChannelVisible] =
+    React.useState<boolean>(false);
+  const [isDirectMessageVisible, setIsDirectMessageVisible] =
+    React.useState<boolean>(false);
+  const [selectedChat, setSelectedChat] = React.useState<any>({
+    name: "general",
+    isPrivate: false,
+    id: "sVblA0J0WXMePBS3Chy9",
+  });
 
-	const fetchData = async () => {
-		setIsLoading(true);
-		const workSpaceRes = await getDoc(
-			doc(db, "workspace-collection", props.workspaceId)
-		);
-		const userResponse = await getDoc(doc(db, "users", props.userId));
-		setUser(userResponse.data());
-		setIsLoading(false);
-		setWorkspace(workSpaceRes.data());
-	};
+  const fetchData = async () => {
+    setIsLoading(true);
+    const workSpaceRes = await getDoc(
+      doc(db, "workspace-collection", props.workspaceId)
+    );
+    const userResponse = await getDoc(doc(db, "users", props.userId));
+    setUser(userResponse.data());
+    setIsLoading(false);
+    setWorkspace(workSpaceRes.data());
+  };
 
-	React.useEffect(() => {
-		fetchData();
-	}, [props.workspaceId, props.userId]);
+  React.useEffect(() => {
+    fetchData();
+  }, [props.workspaceId, props.userId]);
 
-	const fetchChanelData = async (id: string) => {
-		const unsub = onSnapshot(doc(db, "channels", id), (doc) => {
-			console.log("Current data: ", doc.data());
-			setChatData(doc.data());
-		});
-		// const res = await getDoc(doc(db, "channels", id));
-		// console.log("apiCall", res.data());
-		// setChatData(res.data());
-	};
+  const fetchChanelData = async (id: string) => {
+    const res = await getDoc(doc(db, "channels", id));
+    console.log("apiCall", res.data());
+    setChatData(res.data());
+  };
 
-	React.useEffect(() => {
-		fetchChanelData(selectedChat.id);
-	}, []);
+  React.useEffect(() => {
+    fetchChanelData(selectedChat.id);
+  }, []);
 
-	if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
-	return (
-		<div className="h-full bg-black text-text_color ">
-			<header className="h-[60px] w-[100%] bg-dark_header flex items-center border-b border-border_color ">
-				<span className=" w-[100%] flex items-center justify-center">
-					<span className="material-symbols-outlined text-[1.3rem] mr-[20px] text-white">
-						schedule
-					</span>
-					<input
-						placeholder="Search..."
-						className="w-[40%] h-[30px] rounded-md bg-[#3e3d42] text-sm pl-4  "
-					/>
-				</span>
-				<span className="absolute right-0  mr-2">
-					<div className="flex flex-row items-center gap-4">
-						<span className="material-symbols-outlined text-[1.5rem] text-white">
-							help
-						</span>
-						<span>
-							{user && user.avatar && (
-								<img
-									className="w-[30px] h-[30px] object-fit rounded-full"
-									src={user.avatar}
-								/>
-							)}
-						</span>
-					</div>
-				</span>
-			</header>
-			<section className="flex bg-chat_section_color h-[calc(100%-60px)]">
-				<span className="flex flex-[0.2] bg-side_nav  min-w-[250px] border-r border-border_color flex-col ">
-					<div
-						id="workspace-name-container"
-						className="h-[60px] w-[100%] flex justify-between items-center px-3 border-b border-border_color"
-					>
-						<div className="flex flex-row items-center ">
-							<span className="text-l font-bold ">{workspace.name}</span>
-							<BiChevronDown className="text-2xl font-bold " />
-						</div>
-						<span className="bg-white text-black text-xl w-[30px] h-[30px] flex justify-center items-center rounded-full">
-							<FaEdit className="text-sm" />
-						</span>
-					</div>
-					<div id="channels-container">
-						<div
-							id="channel-title"
-							className="mt-2"
-						>
-							<div className=" px-3 py-1">
-								<div className="mt-[1rem] flex flex-row items-center gap-2">
-									<span
-										className=" p-[2px] cursor-pointer rounded hover:bg-hover_color "
-										onClick={() => setIsChannelVisible(!isChannelVisible)}
-									>
-										{isChannelVisible ? (
-											<BsCaretDownFill />
-										) : (
-											<BsCaretRightFill />
-										)}
-									</span>
-									<span className="hover:bg-hover_color px-2 rounded cursor-pointer text-sm font-bold flex flex-row items-center">
-										Channels
-									</span>
-								</div>
-							</div>
-						</div>
+  console.log("Data: ", chatData);
 
-						{/* channel list  */}
-						{isChannelVisible &&
-							workspace.channels.map((channel: any, ind: number) => {
-								return (
-									<div
-										id="channel-title"
-										className="mt-2 cursor-pointer  "
-										key={ind}
-									>
-										<div className=" px-3">
-											<div
-												className="flex flex-row items-center gap-2 py-1 hover:bg-hover_color rounded-md "
-												onClick={() => {
-													setSelectedChat(channel);
-													fetchChanelData(channel.id);
-												}}
-											>
-												<span className=" p-[2px]  rounded ">
-													{channel.isPrivate ? (
-														<BsFillLockFill className="text-sm" />
-													) : (
-														<BsHash className="text-xl" />
-													)}
-												</span>
-												<span className="px-2 rounded  text-sm  ">
-													{channel.name}
-												</span>
-											</div>
-										</div>
-									</div>
-								);
-							})}
-					</div>
+  return (
+    <div className="h-full bg-black text-text_color ">
+      <header className="h-[60px] w-[100%] bg-dark_header flex items-center border-b border-border_color ">
+        <span className=" w-[100%] flex items-center justify-center">
+          <span className="material-symbols-outlined text-[1.3rem] mr-[20px] text-white">
+            schedule
+          </span>
+          <input
+            placeholder="Search..."
+            className="w-[40%] h-[30px] rounded-md bg-[#3e3d42] text-sm pl-4  "
+          />
+        </span>
+        <span className="absolute right-0  mr-2">
+          <div className="flex flex-row items-center gap-4">
+            <span className="material-symbols-outlined text-[1.5rem] text-white">
+              help
+            </span>
+            <span>
+              {user && user.avatar && (
+                <img
+                  className="w-[30px] h-[30px] object-fit rounded-full"
+                  src={user.avatar}
+                />
+              )}
+            </span>
+          </div>
+        </span>
+      </header>
+      <section className="flex bg-chat_section_color h-[calc(100%-60px)]">
+        <span className="flex flex-[0.2] bg-side_nav  min-w-[250px] border-r border-border_color flex-col ">
+          <div
+            id="workspace-name-container"
+            className="h-[60px] w-[100%] flex justify-between items-center px-3 border-b border-border_color"
+          >
+            <div className="flex flex-row items-center ">
+              <span className="text-l font-bold ">{workspace.name}</span>
+              <BiChevronDown className="text-2xl font-bold " />
+            </div>
+            <span className="bg-white text-black text-xl w-[30px] h-[30px] flex justify-center items-center rounded-full">
+              <FaEdit className="text-sm" />
+            </span>
+          </div>
+          <div id="channels-container">
+            <div id="channel-title" className="mt-2">
+              <div className=" px-3 py-1">
+                <div className="mt-[1rem] flex flex-row items-center gap-2">
+                  <span
+                    className=" p-[2px] cursor-pointer rounded hover:bg-hover_color "
+                    onClick={() => setIsChannelVisible(!isChannelVisible)}
+                  >
+                    {isChannelVisible ? (
+                      <BsCaretDownFill />
+                    ) : (
+                      <BsCaretRightFill />
+                    )}
+                  </span>
+                  <span className="hover:bg-hover_color px-2 rounded cursor-pointer text-sm font-bold flex flex-row items-center">
+                    Channels
+                  </span>
+                </div>
+              </div>
+            </div>
 
-					<div id="direct-message-container">
-						<div
-							id="direct-message-title"
-							className="mt-2"
-						>
-							<div className=" px-3 py-1">
-								<div className="mt-[1rem] flex flex-row items-center gap-2">
-									<span
-										className=" p-[2px] cursor-pointer rounded hover:bg-hover_color"
-										onClick={() =>
-											setIsDirectMessageVisible(!isDirectMessageVisible)
-										}
-									>
-										{isDirectMessageVisible ? (
-											<BsCaretDownFill />
-										) : (
-											<BsCaretRightFill />
-										)}
-									</span>
-									<span className="hover:bg-hover_color px-2 rounded cursor-pointer text-sm font-bold flex flex-row">
-										Direct Messages
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</span>
+            {/* channel list  */}
+            {isChannelVisible &&
+              workspace.channels.map((channel: any, ind: number) => {
+                return (
+                  <div
+                    id="channel-title"
+                    className="mt-2 cursor-pointer  "
+                    key={ind}
+                  >
+                    <div className=" px-3">
+                      <div
+                        className="flex flex-row items-center gap-2 py-1 hover:bg-hover_color rounded-md "
+                        onClick={() => {
+                          setSelectedChat(channel);
+                          fetchChanelData(channel.id);
+                        }}
+                      >
+                        <span className=" p-[2px]  rounded ">
+                          {channel.isPrivate ? (
+                            <BsFillLockFill className="text-sm" />
+                          ) : (
+                            <BsHash className="text-xl" />
+                          )}
+                        </span>
+                        <span className="px-2 rounded  text-sm  ">
+                          {channel.name}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
 
-				{/* Chat section */}
-				<span className="flex flex-col gap-[2rem] flex-[1] justify-between relative overflow-y-scroll">
-					<header
-						id="chat-section-header"
-						className="bg-side_nav sticky top-0 p-[1rem_2rem] w-full h-[60px]  border-b border-border_color w-[100%] flex justify-between items-center"
-					>
-						<div className="flex flex-row items-center gap-[5px]">
-							<span>{selectedChat.name}</span>
-							<BiChevronDown />
-						</div>
-						<span>
-							<BiPhoneCall />
-						</span>
-					</header>
+          <div id="direct-message-container">
+            <div id="direct-message-title" className="mt-2">
+              <div className=" px-3 py-1">
+                <div className="mt-[1rem] flex flex-row items-center gap-2">
+                  <span
+                    className=" p-[2px] cursor-pointer rounded hover:bg-hover_color"
+                    onClick={() =>
+                      setIsDirectMessageVisible(!isDirectMessageVisible)
+                    }
+                  >
+                    {isDirectMessageVisible ? (
+                      <BsCaretDownFill />
+                    ) : (
+                      <BsCaretRightFill />
+                    )}
+                  </span>
+                  <span className="hover:bg-hover_color px-2 rounded cursor-pointer text-sm font-bold flex flex-row">
+                    Direct Messages
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </span>
 
-					{chatData && (
-						<ChatModule
-							userId={props.userId}
-							channelId={selectedChat.id}
-							conversations={chatData.conversations}
-						/>
-					)}
-				</span>
-			</section>
-		</div>
-	);
+        {/* Chat section */}
+        <span className="flex flex-col gap-[2rem] flex-[1] justify-between">
+          <header
+            id="chat-section-header"
+            className="p-[1rem_2rem] w-full h-[60px]  border-b border-border_color w-[100%] flex justify-between items-center"
+          >
+            <div className="flex flex-row items-center gap-[5px]">
+              <span>{selectedChat.name}</span>
+              <BiChevronDown />
+            </div>
+            <span>
+              <BiPhoneCall />
+            </span>
+          </header>
+
+          {chatData && <ChatModule conversations={chatData.conversations} />}
+        </span>
+      </section>
+    </div>
+  );
 }
--- src/components/utils/backend.tsx
+++ src/components/utils/backend.tsx
@@ -1,125 +1,208 @@
 import { db } from "../../firebaseConfig";
 import {
-	doc,
-	updateDoc,
-	arrayUnion,
-	addDoc,
-	collection,
-	query,
-	where,
-	getDocs,
-	getDoc,
+  doc,
+  updateDoc,
+  arrayUnion,
+  addDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  onSnapshot,
 } from "firebase/firestore";
 
 // function creates a new Channel
 export const create_channel = async (
-	workspaceId: string,
-	isPrivate: boolean,
-	name: string,
-	usersEmail: string[]
+  workspaceId: string,
+  isPrivate: boolean,
+  name: string,
+  usersEmail: string[]
 ) => {
-	const channel_ref = collection(db, "channels");
-	const user_ref = collection(db, "users");
+  const channel_ref = collection(db, "channels");
+  const user_ref = collection(db, "users");
 
-	const get_user_ids = query(user_ref, where("email", "in", usersEmail));
-	const querySnapshot = await getDocs(get_user_ids);
+  const get_user_ids = query(user_ref, where("email", "in", usersEmail));
+  const querySnapshot = await getDocs(get_user_ids);
 
-	let userIds: string[] = [];
-	querySnapshot.forEach((doc) => {
-		userIds.push(doc.id);
-	});
+  let userIds: string[] = [];
+  querySnapshot.forEach((doc) => {
+    userIds.push(doc.id);
+  });
 
-	const channel_initial_data = {
-		conversations: [],
-		users: userIds,
-	};
+  const channel_initial_data = {
+    conversations: [],
+    users: userIds,
+  };
 
-	//  initialling channel in channel Table
-	const res = await addDoc(channel_ref, channel_initial_data);
+  //  initialling channel in channel Table
+  const res = await addDoc(channel_ref, channel_initial_data);
 
-	//  updating channel in respective workspace table
-	const wrokspace_ref = doc(db, "workspace-collection", workspaceId);
-	const workspace_chennel_data = {
-		id: res.id,
-		isPrivate: isPrivate,
-		name: name,
-	};
-	await updateDoc(wrokspace_ref, {
-		channels: arrayUnion(workspace_chennel_data),
-	});
+  //  updating channel in respective workspace table
+  const wrokspace_ref = doc(db, "workspace-collection", workspaceId);
+  const workspace_chennel_data = {
+    id: res.id,
+    isPrivate: isPrivate,
+    name: name,
+  };
+  await updateDoc(wrokspace_ref, {
+    channels: arrayUnion(workspace_chennel_data),
+  });
 };
 
 // functions accept channels Id and returns the respctive users Details
 export const get_user_details_in_channel = async (channelId: string) => {
-	const docRef = doc(db, "channels", channelId);
-	const docSnap = await getDoc(docRef);
-	let user_data: any = [];
-	if (docSnap.exists()) {
-		// getting all usersId in the channel table
-		let usersIds = docSnap.data().users;
-		console.log(usersIds);
-		const user_ref = collection(db, "users");
+  const docRef = doc(db, "channels", channelId);
+  const docSnap = await getDoc(docRef);
+  let user_data: any = [];
+  if (docSnap.exists()) {
+    // getting all usersId in the channel table
+    let usersIds = docSnap.data().users;
+    console.log(usersIds);
+    const user_ref = collection(db, "users");
 
-		const get_user_ids = query(user_ref, where("id", "in", usersIds));
-		// getting all users Details using userId in
-		const querySnapshot = await getDocs(get_user_ids);
+    const get_user_ids = query(user_ref, where("id", "in", usersIds));
+    // getting all users Details using userId in
+    const querySnapshot = await getDocs(get_user_ids);
 
-		querySnapshot.forEach((doc) => {
-			user_data.push({
-				avatar: doc.data().avatar,
-				name: doc.data().name,
-				status: doc.data().status,
-			});
-		});
+    querySnapshot.forEach((doc) => {
+      user_data.push({
+        avatar: doc.data().avatar,
+        name: doc.data().name,
+        status: doc.data().status,
+      });
+    });
 
-		return user_data;
-	} else {
-		console.log("No such document!");
-	}
+    return user_data;
+  } else {
+    console.log("No such document!");
+  }
 };
 
 // function allow users to send messages in smack channel
 export const send_message_in_channel = async (
-	channelId: string,
-	userId: string,
-	messageText: string
+  channelId: string,
+  userId: string,
+  messageText: string
 ) => {
-	const channel_collection = doc(db, "channels", channelId);
+  const channel_collection = doc(db, "channels", channelId);
 
-	let date = new Date();
-	const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
-		.toString()
-		.padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")} ${date
-		.getHours()
-		.toString()
-		.padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date
-		.getSeconds()
-		.toString()
-		.padStart(2, "0")}.${date.getMilliseconds().toString().padStart(3, "0")}`;
+  let date = new Date();
+  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")} ${date
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")}.${date.getMilliseconds().toString().padStart(3, "0")}`;
 
-	const messageData = {
-		authorId: userId,
-		dateOfCreation: formattedDate,
-		message: messageText,
-		reactions: [],
-		threads: [],
-	};
+  const messageData = {
+    authorId: userId,
+    dateOfCreation: formattedDate,
+    message: messageText,
+    reactions: [],
+    threads: [],
+  };
 
-	try {
-		const res = await updateDoc(channel_collection, {
-			conversations: arrayUnion(messageData),
-		});
-		console.log("message sent successfully!");
-	} catch (err) {
-		console.error(err, "error in addDirectMessage");
-	}
+  try {
+    await updateDoc(channel_collection, {
+      conversations: arrayUnion(messageData),
+    });
+    console.log("message sent successfully!");
+  } catch (err) {
+    console.error(err, "error in addDirectMessage");
+  }
+};
+
+const get_user_details = async (userId: string) => {
+  onSnapshot(doc(db, "users", userId), (doc) => {
+    return doc.data();
+  });
 };
 
 // function creates direct message
-export const create_direct_message = async (user1: string, user2: string) => {
-	const dms_ref = collection(db, "dms");
-	const dms_initial_data = {
-		conversations: [],
-		members: [user1, user2],
-	};
+export const create_direct_message = async (
+  user1: string,
+  user2: string,
+  workspaceId: string
+) => {
+  const dms_ref = collection(db, "dms");
+  const dms_initial_data = {
+    conversations: [],
+    members: [user1, user2],
+  };
+
+  //  initialling direct message in direct message (dms) Table
+  await addDoc(dms_ref, dms_initial_data);
+
+  // Atomically add a new direct message to the "directmessage" array field.
+  const workspace_ref = doc(db, "workspace-collection", workspaceId);
+
+  const user1_details: any = get_user_details(user1);
+  const user2_details: any = get_user_details(user2);
+
+  const workspace_direct_message_data_user1 = {
+    // senders id ie.user1
+    id: user1,
+    // recipients avatar
+    avatar: user2_details["avatar"],
+    // recipients status,
+    status: user2_details["status"],
+  };
+  await updateDoc(workspace_ref, {
+    directMessages: arrayUnion(workspace_direct_message_data_user1),
+  });
+
+  const workspace_direct_message_data_user2 = {
+    // senders id ie.user2
+    id: user2,
+    // recipients avatar
+    avatar: user1_details["avatar"],
+    // recipients status,
+    status: user1_details["status"],
+  };
+  await updateDoc(workspace_ref, {
+    directMessages: arrayUnion(workspace_direct_message_data_user2),
+  });
+};
+
+// add workspaces to user (userId,workspaceId)
+
+export const add_workspace_to_user = async (
+  userId: string,
+  workspaceId: string
+) => {
+  const user_ref = doc(db, "users", userId);
+
+  const docSnap: any = await getDoc(user_ref);
+  const workspace_list = docSnap.data()["workspaces"];
+  if (workspace_list.includes(workspaceId)) return;
+  await updateDoc(user_ref, {
+    workspace: arrayUnion(workspaceId),
+  });
+};
+
+// get workspaces from user return name workspace ,id workspace
+
+export const get_workspaces_from_user = async (userId: string) => {
+  const user_ref = doc(db, "users", userId);
+  const docSnap: any = await getDoc(user_ref);
+  const workspace_list = docSnap.data()["workspaces"];
+
+  const get_workspace_details = query(
+    collection(db, "workspaces"),
+    where("magicLink", "in", workspace_list)
+  );
+  const querySnapshot = await getDocs(get_workspace_details);
+
+  let workspaceId_data: any = [];
+  querySnapshot.forEach((doc) => {
+    workspaceId_data.push({
+      id: doc.data().id,
+      name: doc.data().name,
+    });
+  });
+  return workspaceId_data;
 };
